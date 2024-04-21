c6cc619c1aa68a4c9dafe4245c902f44878ff798
nooblette <min9hyuk@gmail.com>
2024-04-21 16:48:34 +0900

    feat : sec 02. Batch Job 실행 - 프로젝트 생성

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-batch-guide
 향로님의 Spring Batch 가이드를 보고 공부한 내용입니다.
+> 출처 : https://jojoldu.tistory.com/category/Spring%20Batch
---
+++ src/test/java/springbatch/guide/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package springbatch.guide.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
